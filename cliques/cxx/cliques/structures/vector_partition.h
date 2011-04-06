71b85b2447d306061948d19310001887980e2821
zt209 <zt209@90eb4c30-7116-49a8-be4d-672e6860da85>
2011-04-06 20:21:39 +0000

    basic vector partition
    git-svn-id: svn+ssh://ch-sny-data.ch.ic.ac.uk/home/shared-folder/svn/graph-codes@66 90eb4c30-7116-49a8-be4d-672e6860da85

---
+++ cliques/cxx/cliques/structures/vector_partition.h
@@ -0,0 +1,97 @@
+/* Copyright (c) Z Tavares, M Schaub - 2010-2011 */
+#ifndef VECTOR_PARTITION_H
+#define VECTOR_PARTITION_H
+
+#include <vector>
+#include <map>
+
+namespace cliques {
+/**
+ @brief  A partition where the position in the vector denotes the node_id
+ and its value determines its set id.
+ Good when you need constant time assignment of a node to a set
+ and removal of a node from a set
+ */
+class VectorPartition {
+private:
+	int num_nodes;
+	std::vector<int> partition_vector;
+
+public:
+	//#################### CONSTRUCTORS ####################
+
+	// construct empty partition
+	explicit VectorPartition(int num_nodes) :
+		num_nodes(num_nodes), partition_vector(std::vector<int>(num_nodes, -1)) {
+	}
+
+	// construct partition from vector
+	explicit VectorPartition(std::vector<int> partition) :
+		partition_vector(partition) {
+	}
+
+	void initialise_as_singletons() {
+		for (std::vector<int>::iterator itr = partition_vector.begin(); itr
+				!= partition_vector.end(); ++itr) {
+			*itr = itr - partition_vector.begin();
+		}
+	}
+
+	//#################### PUBLIC METHODS ####################
+public:
+	int find_set(int node_id) const {
+		return partition_vector[node_id];
+	}
+
+	void isolate_node(int node_id) {
+		partition_vector[node_id] = -1;
+	}
+
+	void add_node_to_set(int node_id, int set_id) {
+		partition_vector[node_id] = set_id;
+	}
+
+	void normalise_ids() {
+		int start_num = 0;
+		std::map<int,int> set_to_node;
+		for (std::vector<int>::iterator itr = partition_vector.begin(); itr
+						!= partition_vector.end(); ++itr) {
+			std::map<int,int> set_old_to_new;
+			std::map<int,int>::iterator old_set = set_old_to_new.find(*itr);
+			if (old_set == set_to_node.end()) {
+				set_old_to_new[*itr] = start_num;
+				*itr = start_num;
+				start_num++;
+			}
+			else {
+				*itr = old_set->second;
+			}
+		}
+	}
+
+	//#################### ITERATORS ####################
+	class NodeIterator;
+
+	class PartIterator {
+		int alpha;
+	};
+
+	PartIterator begin() {
+		PartIterator alpha;
+		return alpha;
+	}
+
+	PartIterator end() {
+		PartIterator alpha;
+		return alpha;
+	}
+
+	class NodeIterator {
+		int alpha;
+	};
+
+};
+
+}
+
+#endif
