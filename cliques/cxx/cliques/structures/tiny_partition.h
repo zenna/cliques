48e7c90226eef549c6f3dd809cfa022f6b733346
zt209 <zt209@90eb4c30-7116-49a8-be4d-672e6860da85>
2011-04-21 15:39:44 +0000

    work on tiny
    git-svn-id: svn+ssh://ch-sny-data.ch.ic.ac.uk/home/shared-folder/svn/graph-codes@108 90eb4c30-7116-49a8-be4d-672e6860da85

--- cliques/cxx/cliques/structures/tiny_partition.h
+++ cliques/cxx/cliques/structures/tiny_partition.h
@@ -27,27 +27,21 @@ public:
 
 	// construct empty partition
 	explicit TinyPartition(int num_nodes) :
-		num_nodes(num_nodes) {
-    	bits_per_node = ceil(float(std::log(num_nodes) / std::log(2)));
-    	float total_bits_required = bits_per_node * num_node;
-    	int num_chars_required = total_bits_required / (sizeof(unsigned char) * 8);
-    	partition_array = new unsigned char(num_chars_required);
+	num_nodes(num_nodes) {
+		bits_per_node = ceil(float(std::log(num_nodes) / std::log(2)));
+		float total_bits_required = bits_per_node * num_node;
+		int num_chars_required = total_bits_required / (sizeof(unsigned char) * 8);
+		partition_array = new unsigned char(num_chars_required);
 	}
 
 	~TinyPartition() {
 		delete[] partition_array;
 	}
 
-	// construct partition from vector
-	explicit TinyPartition(std::vector<int> partition) :
-		partition_vector(partition) {
-	}
 	// what is that for a horribly over complicated syntax?? --M
 	void initialise_as_singletons() {
-
-		for (std::vector<int>::iterator itr = partition_vector.begin(); itr
-				!= partition_vector.end(); ++itr) {
-			*itr = itr - partition_vector.begin();
+		for (int i=0; i<num_nodes; ++i) {
+			add_node_to_set(i,i);
 		}
 	}
 
@@ -69,31 +63,36 @@ public:
 	int find_set(int node_id) const {
 		// right shift
 		// & with
+		// Convert to int
 		num_bits_to_rightshift = (num_nodes - 1) * bits_per_node;
 		return partition_vector[node_id];
 	}
 
 	void isolate_node(int node_id) {
-		// Either assign to magic value - constant time and quick
-			// BUT requires extra storage and not able to isolate
-			// more than one node before reassignment
-		// Check for free values
-			// But linear time operation in number of nodes
-		partition_vector[node_id] = -1;
+		std::vector<int> set_seen_frequency (num_nodes, 0);
+
+		for (int i=0; i<num_nodes; ++i) {
+			set_seen_frequency[i]++;
+		}
+		std::vector<int>::iterator set_itr = set_seen_frequency.find(0);
+
+		int available_set = set_itr - set_seen_frequency.begin();
+
+		add_node_to_set(node_id, available_set);
 	}
 
 	void add_node_to_set(int node_id, int set_id) {
 		// Create mask with all 1s except for 0s where node I want to change is
 		// Create mask w
 		for (int i=0<)
-		partition_vector[node_id] = set_id;
+			partition_vector[node_id] = set_id;
 	}
 
 	void normalise_ids() {
 		int start_num = 0;
 		std::map<int, int> set_old_to_new;
 		for (std::vector<int>::iterator itr = partition_vector.begin(); itr
-				!= partition_vector.end(); ++itr) {
+		!= partition_vector.end(); ++itr) {
 
 			std::map<int, int>::iterator old_set = set_old_to_new.find(*itr);
 			if (old_set == set_old_to_new.end()) {
@@ -107,36 +106,17 @@ public:
 	}
 
 	int element_count() {
-		return partition_vector.size();
+		return num_nodes;
 	}
 
 	int set_count() {
-		std::set<int> seen_nodes;
-		for (std::vector<int>::iterator itr = partition_vector.begin(); itr
-						!= partition_vector.end(); ++itr) {
-			if (*itr != -1) {
-				seen_nodes.insert(*itr);
-			}
-		}
-		return seen_nodes.size();
-		/*
+		std::set<int> seen_sets;
 
-		std::vector<int> count_vector(num_nodes, 0);
-		for (std::vector<int>::iterator itr = partition_vector.begin(); itr
-				!= partition_vector.end(); ++itr) {
-			if (*itr == -1) {
-				// TODO warning: there are unassigned nodes
-				std::cout << "The element has no set assigned" << std::endl;
-				return 0;
-			} else
-				count_vector[*itr] = 1;
+		for (int i=0; i<num_nodes; ++i) {
+			int seen_set = find_set(i);
+			seen_sets.insert(seen_set);
 		}
-		int count = 0;
-		for (std::vector<int>::iterator itr = count_vector.begin(); itr
-				!= count_vector.end(); ++itr) {
-			count += *itr;
-		}*/
-		//return count;
+		return seen_sets.size();
 	}
 
 	//#################### ITERATORS ####################
