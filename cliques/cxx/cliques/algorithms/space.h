e276f000093c7dd076f8e8520e008db22538da61
zt209 <zt209@90eb4c30-7116-49a8-be4d-672e6860da85>
2011-04-27 12:20:00 +0000

    find neighbours of a partition
    git-svn-id: svn+ssh://ch-sny-data.ch.ic.ac.uk/home/shared-folder/svn/graph-codes@120 90eb4c30-7116-49a8-be4d-672e6860da85

--- cliques/cxx/cliques/algorithms/space.h
+++ cliques/cxx/cliques/algorithms/space.h
@@ -7,29 +7,43 @@
 namespace cliques {
 
 /**
- @brief  Finds neighbours of a partition
+ @brief  Find all (single node moveset) neighbours of a partition
 
+ Finds neighbours of a partition where a neighbour is a partition
+ which can be created by moving one node into an adjacent group
+ or by isolating it into its own group.
+
+ @param[in] all_partitons reference to unordered set of partitions
+ @param[out] space output graph representing the space
  */
 template<typename G, typename P>
-void find_neighbours(G &graph, P &partition,
-		boost::unordered_set<P, cliques::partition_hash,
-				cliques::partition_equal> &neighbour_partitions) {
-	P temp_partition = partition;
+void find_neighbours(G &graph, P const &partition,
+        boost::unordered_set<P, cliques::partition_hash,
+                cliques::partition_equal> &neighbour_partitions) {
+    P temp_partition = partition;
 
-	typedef typename G::EdgeIt EdgeIt;
+    typedef typename G::EdgeIt EdgeIt;
 
-	for (EdgeIt edge(graph); edge != lemon::INVALID; ++edge) {
-		//
-		Node n1 = graph.u(edge);
-		Node n2 = graph.v(edge);
-		temp_partition.isolate_node(u);
-		temp_partition.isolate_node(v);
+    for (EdgeIt edge(graph); edge != lemon::INVALID; ++edge) {
+        int n1 = graph.id(graph.u(edge));
+        int n2 = graph.id(graph.u(edge));
+        int set_of_n1 = partition.find_set(n1);
+        int set_of_n2 = partition.find_set(n2);
 
-		int set_of_n1 = partition.find_set(n1);
-		int set_of_n2 = partition.find_set(n2);
-		partition.add_node_to_set(n1, set_of_n2);
-		partition.add_node_to_set(n2, set_of_n1);
-	}
+        //Add partition with n1 isolated and in n2's set, then undo
+        temp_partition.isolate_node(n1);
+        neighbour_partitions.insert(temp_partition);
+        temp_partition.add_node_to_set(n1, set_of_n2);
+        neighbour_partitions.insert(temp_partition);
+        temp_partition.add_node_to_set(n1, set_of_n1);
+
+        // Do vice versa for n2
+        temp_partition.isolate_node(n2);
+        neighbour_partitions.insert(temp_partition);
+        temp_partition.add_node_to_set(n2, set_of_n1);
+        neighbour_partitions.insert(temp_partition);
+        temp_partition.add_node_to_set(n1, set_of_n1);
+    }
 }
 
 /**
@@ -44,43 +58,46 @@ void find_neighbours(G &graph, P &partition,
  @param[out] space output graph representing the space
  */
 template<typename G, typename P>
-void create_space(boost::unordered_set<P, cliques::partition_hash,
-		cliques::partition_equal> &all_partitions, G &space) {
+void create_space(G &graph, boost::unordered_set<P, cliques::partition_hash,
+        cliques::partition_equal> &all_partitions, G &space) {
 
-	typedef typename G::Node Node;
-	typedef typename G::Edge Edge;
-	typedef boost::unordered_set<P, cliques::partition_hash,
-			cliques::partition_equal> partition_set;
-	typedef typename boost::unordered_set<P, cliques::partition_hash,
-			cliques::partition_equal>::iterator partition_set_itr;
+    typedef typename G::Node Node;
+    typedef typename G::Edge Edge;
+    typedef typename boost::unordered_set<P, cliques::partition_hash,
+            cliques::partition_equal> partition_set;
+    typedef typename boost::unordered_set<P, cliques::partition_hash,
+            cliques::partition_equal>::iterator partition_set_itr;
 
-	boost::unordered_map<P, Node, cliques::partition_hash,
-			cliques::partition_equal> partition_to_node;
+    boost::unordered_map<P, Node, cliques::partition_hash,
+            cliques::partition_equal> partition_to_spacenode;
 
-	// Create node in space for each partition
-	for (partition_set_itr itr = all_partitions.begin(); itr
-			!= all_partitions.end(); ++itr) {
-		Node temp_node = space.addNode();
-		//partition_to_graph[temp_node] =
-	}
+    // Create node in space for each partition
+    for (partition_set_itr itr = all_partitions.begin(); itr
+            != all_partitions.end(); ++itr) {
+        Node temp_node = space.addNode();
+        partition_to_spacenode[*itr] = temp_node;
+    }
 
-	for (partition_set_itr itr = all_partitions.begin(); itr
-			!= all_partitions.end(); ++itr) {
+    for (partition_set_itr itr = all_partitions.begin(); itr
+            != all_partitions.end(); ++itr) {
 
-		Node current_node = partition_to_node[*itr];
-		partition_set neighbour_partitions;
-		cliques::find_neighbours(*itr, neighbour_partitions);
+        Node current_node = partition_to_spacenode[*itr];
+        partition_set neighbour_partitions;
+        cliques::find_neighbours(graph, *itr, neighbour_partitions);
 
-		for (partition_set_itr itr = neighbour_partitions.begin(); itr
-				!= all_partitions.end(); ++itr) {
-			Node neighbour_node = partition_to_node[*itr];
-			// CHECK NOT SAME PARTITION
-			Edge e = lemon::findEdge(space, current_node, neighbour_node);
-			if (e == lemon::INVALID) {
-				Edge e = space.addEdge(current_node, neighbour_node);
-			}
-		}
-	}
+        for (partition_set_itr neigh_itr = neighbour_partitions.begin(); neigh_itr
+                != neighbour_partitions.end(); ++neigh_itr ) {
+            Node neighbour_node = partition_to_spacenode[*itr];
+
+            // CHECK NOT SAME PARTITION
+            if (*itr != *neigh_itr) {
+                Edge e = lemon::findEdge(space, current_node, neighbour_node);
+                if (e == lemon::INVALID) {
+                    Edge e = space.addEdge(current_node, neighbour_node);
+                }
+            }
+        }
+    }
 }
 
 template<typename G>
