ef0882c91ecfa3fca7eef25e9184865d90314316
apd09 <apd09@90eb4c30-7116-49a8-be4d-672e6860da85>
2011-05-12 14:20:01 +0000

    Effectively added the matlab folder now (hopefully)
    git-svn-id: svn+ssh://ch-sny-data.ch.ic.ac.uk/home/shared-folder/svn/graph-codes@140 90eb4c30-7116-49a8-be4d-672e6860da85

---
+++ cliques/matlab/Install_Stability.m
@@ -0,0 +1,35 @@
+function [] = Install_Stability(clique_path)
+
+if ~isdir(clique_path)
+    error('The path provided is not a folder. Please provide the path of the source code directory of Clique.');
+end
+
+curr_folder = pwd;
+
+cd(clique_path);
+
+disp('Building of mex files...');
+
+mex -DUSE_BOOST -I./ ./louvain_matlab_interface.cpp
+
+disp('Moving build files to bin directory...');
+
+movefile('louvain_matlab_interface.mex*',curr_folder);
+
+copyfile('stability.m',curr_folder);
+
+copyfile('varinfo.m',curr_folder);
+
+cd(curr_folder);
+
+mexfile = dir('louvain_matlab_interface.mex*');
+
+movefile(mexfile(1).name,['stability_louvain' mexfile(1).name(25:end)]);
+
+disp('Adding bin directory to path...');
+
+path(pwd,path);
+
+savepath;
+
+disp('Done');
