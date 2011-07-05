d0356653ac3c69a61d613ed9f39243247e8f5c7e
mts09 <mts09@90eb4c30-7116-49a8-be4d-672e6860da85>
2011-07-05 16:39:43 +0000

    fish graph
    git-svn-id: svn+ssh://ch-sny-data.ch.ic.ac.uk/home/shared-folder/svn/graph-codes@258 90eb4c30-7116-49a8-be4d-672e6860da85

--- cliques/cxx/cliques/structures/make_graphs.h
+++ cliques/cxx/cliques/structures/make_graphs.h
@@ -4,89 +4,140 @@
 #include <assert.h>
 #include <cstdlib>
 #include <iostream>
+#include <lemon/concepts/graph.h>
 
 namespace cliques {
 
+template<typename G, typename M>
+void make_fish_graph(G &graph, M &weights, double epsilon, bool many_links) {
+	int num_nodes = 8;
+	typedef typename G::Node Node;
+	typedef typename G::Edge Edge;
+	for (int i = 0; i < num_nodes; ++i) {
+		graph.addNode();
+	}
+
+	// fish head
+	for (int i = 0; i < 4; ++i) {
+		for (int j = i + 1; j < 4; ++j) {
+			Node n1 = graph.nodeFromId(i);
+			Node n2 = graph.nodeFromId(i);
+			graph.addEdge(n1, n2);
+		}
+	}
+
+	// tail fin
+	graph.addEdge(graph.nodeFromId(4), graph.nodeFromId(5));
+	graph.addEdge(graph.nodeFromId(4), graph.nodeFromId(6));
+	graph.addEdge(graph.nodeFromId(4), graph.nodeFromId(7));
+	graph.addEdge(graph.nodeFromId(5), graph.nodeFromId(6));
+	graph.addEdge(graph.nodeFromId(6), graph.nodeFromId(7));
+
+	// now make edge weights
+	make_weights_from_edges(graph, weights);
+
+	if (many_links == false) {
+		Edge e = graph.addEdge(graph.nodeFromId(3), graph.nodeFromId(4));
+		weights[e] = epsilon;
+	} else {
+		int Ne = 6;
+		Edge e = graph.addEdge(graph.nodeFromId(3), graph.nodeFromId(4));
+		weights[e] = epsilon / Ne;
+		Edge e = graph.addEdge(graph.nodeFromId(0), graph.nodeFromId(6));
+		weights[e] = epsilon / Ne;
+		Edge e = graph.addEdge(graph.nodeFromId(1), graph.nodeFromId(5));
+		weights[e] = epsilon / Ne;
+		Edge e = graph.addEdge(graph.nodeFromId(2), graph.nodeFromId(7));
+		weights[e] = epsilon / Ne;
+		Edge e = graph.addEdge(graph.nodeFromId(2), graph.nodeFromId(6));
+		weights[e] = epsilon / Ne;
+		Edge e = graph.addEdge(graph.nodeFromId(3), graph.nodeFromId(5));
+		weights[e] = epsilon / Ne;
+	}
+
+}
+
 template<typename G>
 void make_complete_graph(G &graph, int num_nodes) {
-    typedef typename G::NodeIt NodeIt;
+	typedef typename G::NodeIt NodeIt;
 
-    for (int i = 0; i < num_nodes; ++i) {
-        graph.addNode();
-    }
+	for (int i = 0; i < num_nodes; ++i) {
+		graph.addNode();
+	}
 
-    for (NodeIt n1(graph); n1 != lemon::INVALID; ++n1) {
-        for (NodeIt n2 = n1; ++n2 != lemon::INVALID;) {
-            graph.addEdge(n1,n2);
-        }
-    }
+	for (NodeIt n1(graph); n1 != lemon::INVALID; ++n1) {
+		for (NodeIt n2 = n1; ++n2 != lemon::INVALID;) {
+			graph.addEdge(n1, n2);
+		}
+	}
 }
 
 template<typename G, typename M>
 void make_complete_graph(G &graph, int num_nodes, M &weights) {
-    make_complete_graph(graph, num_nodes);
-    make_weights_from_edges(graph, weights);
+	make_complete_graph(graph, num_nodes);
+	make_weights_from_edges(graph, weights);
 }
 
 template<typename G>
 void make_path_graph(G &graph, int num_nodes) {
-    typedef typename G::NodeIt NodeIt;
+	typedef typename G::NodeIt NodeIt;
 
-    for (int i = 0; i < num_nodes; ++i) {
-        graph.addNode();
-    }
-    for (NodeIt n1(graph); n1 != lemon::INVALID; ++n1) {
-        NodeIt n2 = n1;
-        ++n2;
-        if (n2 == lemon::INVALID) {
-            break;
-        }
-        graph.addEdge(n1,n2);
-    }
+	for (int i = 0; i < num_nodes; ++i) {
+		graph.addNode();
+	}
+	for (NodeIt n1(graph); n1 != lemon::INVALID; ++n1) {
+		NodeIt n2 = n1;
+		++n2;
+		if (n2 == lemon::INVALID) {
+			break;
+		}
+		graph.addEdge(n1, n2);
+	}
 }
 
 template<typename G, typename M>
 void make_path_graph(G &graph, int num_nodes, M &weights) {
-    make_path_graph(graph, num_nodes);
-    make_weights_from_edges(graph, weights);
+	make_path_graph(graph, num_nodes);
+	make_weights_from_edges(graph, weights);
 }
 
 template<typename G>
 void make_ring_graph(G &graph, int num_nodes) {
-    typedef typename G::NodeIt NodeIt;
+	typedef typename G::NodeIt NodeIt;
 
-    for (int i = 0; i < num_nodes; ++i) {
-        graph.addNode();
-    }
-    for (NodeIt n1(graph); n1 != lemon::INVALID; ++n1) {
-        NodeIt n2 = n1;
-        ++n2;
-        if (n2 == lemon::INVALID) {
-            graph.addEdge(n1,NodeIt(graph));
-            break;
-        }
-        graph.addEdge(n1,n2);
-    }
+	for (int i = 0; i < num_nodes; ++i) {
+		graph.addNode();
+	}
+	for (NodeIt n1(graph); n1 != lemon::INVALID; ++n1) {
+		NodeIt n2 = n1;
+		++n2;
+		if (n2 == lemon::INVALID) {
+			graph.addEdge(n1, NodeIt(graph));
+			break;
+		}
+		graph.addEdge(n1, n2);
+	}
 }
 
 template<typename G, typename M>
 void make_ring_graph(G &graph, int num_nodes, M &weights) {
-    make_ring_graph(graph, num_nodes);
-    make_weights_from_edges(graph, weights);
+	make_ring_graph(graph, num_nodes);
+	make_weights_from_edges(graph, weights);
 }
 
-template <typename G>
-void make_hierarchical_benchmark_graph(G &graph, double balance, double hierarchy, int num_nodes) {
-    for (int i=0;i<num_nodes;++i) {
-        graph.addNode(i);
-    }
+template<typename G>
+void make_hierarchical_benchmark_graph(G &graph, double balance,
+		double hierarchy, int num_nodes) {
+	for (int i = 0; i < num_nodes; ++i) {
+		graph.addNode(i);
+	}
 }
 
-template <typename G, typename M>
+template<typename G, typename M>
 void make_weights_from_edges(G &graph, M &weights) {
-    for (typename G::EdgeIt e(graph); e!= lemon::INVALID; ++e) {
-        weights[e] = 1.0;
-    }
+	for (typename G::EdgeIt e(graph); e != lemon::INVALID; ++e) {
+		weights[e] = 1.0;
+	}
 }
 
 }
