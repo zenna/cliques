3adcbc76a83adb76eb85fc528e000ff113567490
mts09 <mts09@90eb4c30-7116-49a8-be4d-672e6860da85>
2011-08-16 12:57:47 +0000

    make clique compile again, for mex interface II
    git-svn-id: svn+ssh://ch-sny-data.ch.ic.ac.uk/home/shared-folder/svn/graph-codes@284 90eb4c30-7116-49a8-be4d-672e6860da85

--- cliques/matlab/Install_Stability.m
+++ cliques/matlab/Install_Stability.m
@@ -11,12 +11,15 @@ cd(clique_path);
 
 disp('Building of mex files...');
 
+
+setenv('LD_RUN_PATH','/home/mts09/repositories/group_repository/graph-codes/cliques/cxx/cliques')
+
 mex -v -DUSE_BOOST CXXFLAGS="\$CXXFLAGS -std=gnu++0x" -O -output stability_louvain -I./ ...
-    ./cliques/louvain_matlab_interface.cpp ...
-    /home/mts09/repositories/group_repository/graph-codes/cliques/cxx/cliques/libexpokit.so ...
-    LDFLAGS="\$LDFLAGS /home/mts09/repositories/group_repository/graph-codes/cliques/cxx/cliques/libexpokit.so" ...
-    CXX=g++-4.6
-    %LDCXXFLAGS='\$LDCXXFLAGS /home/mts09/repositories/group_repository/graph-codes/cliques/cxx/cliques/libexpokit.so' ...
+        ./cliques/louvain_matlab_interface.cpp ...
+        CXX=g++-4.6 -lblas -llapack -lemon -lgfortran...
+    /home/mts09/repositories/group_repository/graph-codes/cliques/cxx/cliques/libexpokit.so
+
+%/home/mts09/repositories/group_repository/graph-codes/cliques/cxx/cliques/expokit.f ...
 
 disp('Moving build files to bin directory...');
 
@@ -31,3 +34,11 @@ path(pwd,path);
 savepath;
 
 disp('Done');
+
+
+% Comiled with :
+% g++-4.6 -c  -I./ -I/usr/local/MATLAB/R2010b/extern/include -I/usr/local/MATLAB/R2010b/simulink/include -DMATLAB_MEX_FILE -ansi -D_GNU_SOURCE -fPIC -fno-omit-frame-pointer -pthread -std=gnu++0x  -DUSE_BOOST -DMX_COMPAT_32 -O -DNDEBUG  ./cliques/louvain_matlab_interface.cpp -lemon -L/usr/local/include/lemon
+% 
+% gfortran -c  -I./ -I/usr/local/MATLAB/R2010b/extern/include -I/usr/local/MATLAB/R2010b/simulink/include -fexceptions -fPIC -fno-omit-frame-pointer  -DUSE_BOOST -DMX_COMPAT_32 -O  /home/mts09/repositories/group_repository/graph-codes/cliques/cxx/cliques/expokit.f
+% 
+% g++-4.6 -O -pthread -shared -Wl,--version-script,/usr/local/MATLAB/R2010b/extern/lib/glnxa64/mexFunction.map -Wl,--no-undefined -o  stability_louvain.mexa64  louvain_matlab_interface.o expokit.o -Wl,-rpath-link,/usr/local/MATLAB/R2010b/bin/glnxa64 -L/usr/local/MATLAB/R2010b/bin/glnxa64 -lmx -lmex -lmat -lm -lblas -llapack -lemon -lgfortran
