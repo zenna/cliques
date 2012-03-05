773b88592a429dbabc3178f41afc72950885fedf
mts09 <mts09@90eb4c30-7116-49a8-be4d-672e6860da85>
2012-03-05 18:25:39 +0000

    wrapper script for creating multi-level landscapes
    git-svn-id: svn+ssh://ch-sny-data.ch.ic.ac.uk/home/shared-folder/svn/graph-codes@336 90eb4c30-7116-49a8-be4d-672e6860da85

---
+++ cliques/landscapes_script.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# script for doing all the fancy landscape operations in one go
+
+if [ -z $1 ] ; then
+echo "you need to provide one argument to identify paths"
+exit 0
+fi
+
+if [ $1 = "M" ] ; then
+# Michael's DIR
+DIR_REP="$HOME/repositories/group_repository/graph-codes/cliques"
+DIR_BIN="$HOME/PhD/ext_programmes_and_lib/clique"
+fi
+
+if [ $1 = "Z" ] ; then
+# Zenna's DIR
+DIR_REP="$HOME/repositories/group_repository/graph-codes/cliques"
+DIR_BIN="$HOME/PhD/ext_programmes_and_lib/clique"
+fi
+
+# check if argument 2 is empty if yes use default test graph otherwise use graph provided
+if [ -z $2] ; then
+GRAPH="barbell_n8.edj"
+else 
+GRAPH=$2
+fi
+
+echo `$DIR_BIN/tests/test_louvain $DIR_REP/data/graphs/$GRAPH`
+echo `$DIR_BIN/scripts/create_multilandscape -G $DIR_REP/data/graphs/$GRAPH -I ./intermediate_graphs -H ./optimal_partitions.mat`
+echo `python $DIR_REP/cliques/scripts/create_json.py -x ./out_hierarchy1 -o $DIR_REP/cliques.js/interactive/js/data/$GRAPH.json`
+
+if [ $3 = "R" ] ; then
+	echo `rm out_* intermediate_graphs* optimal_partitions.mat`
+fi 
